baadd263f2e8e7abf5e51c7c614919193c01a458
AnujShah17 <anujsachinshah@gmail.com>
2023-05-26 16:51:18 +0530

    sample data of video-audio

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import simple_colors
 from arduino import flash_detect
 import pandas as pd
 
+from reuseable import serverAppium
+from testScripts import testgoogleFile
 from reuseable.configs import MobileConfig
 from testScripts import testVideo
 from audio import listen
@@ -15,7 +17,10 @@ if __name__ == '__main__':
 
     try:
         print("----pre setting is being executed----")
-        ser,led = preSettings.pre_req()
+        # ser,led = preSettings.pre_req()
+        serverAppium.start_server()
+        ser, led = flash_detect.arduino()
+        time.sleep(2)
         print(simple_colors.green("----pre setting has been finished sucessfully----"))
     except:
         x=False
@@ -24,7 +29,8 @@ if __name__ == '__main__':
     #     iterartion_times = int(input("how many times you wanna play "))
         try:
             print(simple_colors.blue("----Relauching the application----"))
-            testVideo.launch_appium_driver()
+            # testVideo.launch_appium_driver()
+            testgoogleFile.launch_appium_driver()
         except:
             pass
 
@@ -32,8 +38,10 @@ if __name__ == '__main__':
         data1=[]
         # wb, ws, header_format=excel_data.starting_workbook()
 
+
         # print("Starting the thread",i)
-        thread1 = threading.Thread(target=testVideo.play_video)
+        # thread1 = threading.Thread(target=testVideo.play_video)
+        thread1 = threading.Thread(target=testgoogleFile.play_video)
         thread1.start()
         # thread3 = threading.Thread(target=listen.listen)
         # thread3.start()
@@ -48,9 +56,9 @@ if __name__ == '__main__':
         # thread5.start()
         # thread5.join()
         thread1.join()
+        thread6.join()
         thread2.join()
         # thread3.join()
-        thread6.join()
         time.sleep(5)
         print(testVideo.dict)
         c=excel_data.appending(testVideo.dict)
