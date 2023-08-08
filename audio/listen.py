85cdaa05fef7538c0710d08c69db9fe09442353b
AnujShah17 <anujsachinshah@gmail.com>
2023-08-08 15:54:22 +0530

    Changes in listen.py file

--- audio/listen.py
+++ audio/listen.py
@@ -1,28 +1,35 @@
 import threading
-import speech_recognition as sr
-# from testScripts import testVideo
 import time
 import keyboard
-# from reuseable.configs import MobileConfig
+import speech_recognition as sr
+from testScripts import testVideo
+from reuseable.configs import MobileConfig
 
 r = sr.Recognizer()
 m = sr.Microphone()
 f = sr.AudioFile(r"C:\Users\Anuj\PycharmProjects\Project(video-audio)\audio\recorded_audio7.wav")
 
 aud_tup = []
-Threshold_value = 200
-global event
+THRESHOLD_VALUE = 200
 event=threading.Event()
-class audio(object):
-    def adjust(self, event: threading.Event) -> None:
+
+class Audio():
+    """
+    This class provides methods for adjusting audio settings, detecting sound input,
+    and recognizing speech from a microphone source.
+
+    """
+    def adjust(self):
         """
         Continuously adjusts audio for ambient noise.
 
-        This method continuously adjusts the audio for ambient noise using the `adjust_for_ambient_noise` function from
+        This method continuously adjusts the audio for ambient noise using the
+        `adjust_for_ambient_noise` function from
         the `r` object. It runs in a loop until the `event` is set.
 
         Args:
-            event (threading.Event): A threading event that signals when to stop adjusting the audio.
+            event (threading.Event): A threading event that signals when to
+            stop adjusting the audio.
 
         Returns:
             None
@@ -30,15 +37,14 @@ class audio(object):
         while True:
             with m as source:
                 r.adjust_for_ambient_noise(source)
-            if event.is_set():
-                break
-            elif keyboard.is_pressed('insert'):
+            if event.is_set() or keyboard.is_pressed('insert'):
                 break
 
 
     def audio_return(self):
         """
-        Continuously listens for sound input from the microphone and returns the timestamp when a sound is detected.
+        Continuously listens for sound input from the microphone and returns
+        the timestamp when a sound is detected.
 
         Returns:
         float: Timestamp of the sound detection.
@@ -46,12 +52,12 @@ class audio(object):
         Raises:
         IOError: If there is an issue with the microphone.
         """
-        a = 0
+        break_variable = 0
         print("----Initializing microphone----")
         while True:
-            # if a > 800:
-            #     event.set()
-            #     break
+            if break_variable > 800:
+                event.set()
+                break
             if keyboard.is_pressed('insert'):
                 event.set()
                 break
@@ -60,15 +66,15 @@ class audio(object):
             time.sleep(0.00002)
             sound_time = time.time()
             print(r.energy_threshold)
-            if r.energy_threshold >= Threshold_value:
+            if r.energy_threshold >= THRESHOLD_VALUE:
                 tup_audio = (r.energy_threshold, sound_time)
-                # MobileConfig.audio_det.append(tup_audio)
-                a = 0
+                MobileConfig.audio_det.append(tup_audio)
+                break_variable = 0
                 print("True")
                 print("----Timestamp of sound detect:", sound_time, "----")
 
             else:
-                a += 1
+                break_variable += 1
 
     def listen(self):
         """
@@ -77,13 +83,14 @@ class audio(object):
             Returns:
             str: The recognized text from the speech input.
 
-            Raises: SpeechRecognitionError: If the speech input cannot be recognized or if there is an issue with the
+            Raises: SpeechRecognitionError: If the speech input cannot be recognized
+            or if there is an issue with the
             microphone.
         """
         try:
             with m as source:
                 x_current_time = time.time()
-                # testVideo.dict["Listen_start"] = str(x_current_time)[6:]
+                testVideo.dict["Listen_start"] = str(x_current_time)[6:]
                 print("Listen Started....", x_current_time)
                 print("------------------------------------------")
                 audio_data_my = r.listen(source)
@@ -93,7 +100,6 @@ class audio(object):
                 print("------------------------------------------")
             text = r.recognize_google(audio_data_my)
             print("sending data..", time.time())
-            end = time.time()
             print("text:", text)
         except sr.UnknownValueError:
             # Speech recognition could not understand the input
@@ -106,9 +112,17 @@ class audio(object):
 
 
 def audio_p():
-    tt = audio() 
-    y = threading.Thread(target=tt.adjust,args=(event,))
-    y.start()
-    tt.audio_return()
+    """
+    Starts a threaded audio processing routine.
+
+    This function creates an instance of the `Audio` class and initiates two concurrent
+    processes. It starts a new thread to continuously adjust audio for ambient noise
+    using the `adjust` method from the `Audio` instance. Additionally, it calls the
+    `audio_return` method to continuously listen for sound input from the microphone and
+    return the timestamp when a sound is detected."""
+    class_call = Audio()
+    start_thread = threading.Thread(target=class_call.adjust,args=(event,))
+    start_thread.start()
+    class_call.audio_return()
 
 # audio_p()
